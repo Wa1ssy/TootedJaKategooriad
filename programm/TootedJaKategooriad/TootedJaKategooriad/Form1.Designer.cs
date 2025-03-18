784bdae146f54749e3c56821486c8cd9433bf56d
Opilane TTHK <opilane@tthk.ee>
2025-03-18 10:14:17 +0200

    ofcofofof

--- programm/TootedJaKategooriad/TootedJaKategooriad/Form1.Designer.cs
+++ programm/TootedJaKategooriad/TootedJaKategooriad/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TootedJaKategooriad
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dataGridViewCategories = new DataGridView();
+            dataGridViewProducts = new DataGridView();
+            buttonSave = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCategories).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewProducts).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridViewCategories
+            // 
+            dataGridViewCategories.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCategories.Location = new Point(12, 12);
+            dataGridViewCategories.Name = "dataGridViewCategories";
+            dataGridViewCategories.Size = new Size(240, 150);
+            dataGridViewCategories.TabIndex = 0;
+            // 
+            // dataGridViewProducts
+            // 
+            dataGridViewProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewProducts.Location = new Point(258, 12);
+            dataGridViewProducts.Name = "dataGridViewProducts";
+            dataGridViewProducts.Size = new Size(240, 150);
+            dataGridViewProducts.TabIndex = 1;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Location = new Point(423, 168);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(75, 23);
+            buttonSave.TabIndex = 2;
+            buttonSave.Text = "Salvesta";
+            buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(511, 200);
+            Controls.Add(buttonSave);
+            Controls.Add(dataGridViewProducts);
+            Controls.Add(dataGridViewCategories);
+            Name = "MainForm";
+            Text = "Tooted ja Kategooriad";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewCategories).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewProducts).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridViewCategories;
+        private DataGridView dataGridViewProducts;
+        private Button buttonSave;
     }
 }
